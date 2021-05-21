d15894833891a1f89434305ef8ce84a0e4ba332e
Javier Fernández-Coto Córdoba <javier.fernandez@edu.uah.es>
2021-05-21 10:33:46 +0200

    implementación primera plantilla, resuelto error de carga CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LinkedinLoginResponse } from "./linkedinLoginResponse/linkedinLoginResponse.component";
 import { HomeComponent } from './home/home.component';
+import { TemplateAngularComponent } from './templates/templateAngular/templateAngular.component';
+import { TemplateAndroidComponent } from './templates/templateAndroid/templateAndroid.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: "full" },
   { path: "login", component: LoginComponent },
-  { path: "linkedInLogin", component: LinkedinLoginResponse }
+  { path: "linkedInLogin", component: LinkedinLoginResponse },
+  { path: "templateAngular", component: TemplateAngularComponent },
+  { path: "templateAndroid", component: TemplateAndroidComponent }
 ];
 
 @NgModule({
---
+++ src/app/templates/templateAndroid/templateAndroid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templateAndroid',
+  templateUrl: './templateAndroid.component.html',
+  styleUrls: ['./templateAndroid.component.css']
+})
+export class TemplateAndroidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
